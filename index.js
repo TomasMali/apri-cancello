4df0f21c2a87ff24ba845be3c813ea4cc322786b
tommal <tommal@ubuntu.ubuntu-domain>
2020-05-01 13:50:23 +0200

    new

---
+++ index.js
@@ -0,0 +1,73 @@
+
+
+const TelegramBot = require('node-telegram-bot-api');
+// const token = '477553552:AAFPFR-UOeW2ObvIIWp8QCQnOyhGTuOWBVo';
+const token = '1048183703:AAGeeJA0IHd5bEF0k8k5Qq6kgKFtOhAOBmo';
+const bot = new TelegramBot(token, { polling: true });
+exports.bot = bot;
+
+
+const APRI_CANCELLO = "\ud83d\udde3 Apri cancello"
+
+
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(17, 'out'); //use GPIO pin 4 as output
+
+
+
+inline_keyboard = [];
+
+
+// Here starts everything
+bot.onText(/\/start/, (msg) => {
+    var telegramUser = msg.from
+
+    //  $.sendMessage("Utente (" + telegramUser.firstName + " " + telegramUser.lastName + ")  " + body.message)
+    bot.sendMessage(msg.chat.id, "Welcome " + msg.from.first_name + ", registrazione effettuata correttamente! Adesso puoi unirti ad un tavolo usando il menu Cerca tavola.", {
+        "reply_markup": {
+            "keyboard": [
+                [APRI_CANCELLO]
+            ]
+        }
+    });
+});
+
+
+function apri() {
+
+    LED.writeSync(1); //turn LED on or off depending on the button state (0 or 1)
+    setTimeout(() => { LED.writeSync(0); }, 100);
+}
+
+
+// Catch every messagge text 
+bot.on('message', (msg) => {
+
+    //    const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+    //    const LED = new Gpio(17, 'out'); //use GPIO pin 4, and specify that it is output
+
+    if (msg.text.toString() === APRI_CANCELLO) {
+
+        bot.sendMessage(msg.chat.id, "Il cancello 1 è stato aperto!")
+
+        setTimeout(apri, 500);
+
+    }
+    //--
+    else {
+        bot.sendMessage(msg.chat.id, "Commando non riconosciuto!")
+    }
+
+})
+
+
+
+
+
+//*************************************************************************************************************************** */
+//*************************************************************************************************************************** */
+//*************************************************************************************************************************** */
+//*************************************************************************************************************************** */
+//*************************************************************************************************************************** */
+//*************************************************************************************************************************** */
+
